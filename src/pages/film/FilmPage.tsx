fc32215098c153ff2ee1af17622e94ddd05e2f19
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-06-14 19:05:47 +0300

    film page update

--- src/pages/film/FilmPage.tsx
+++ src/pages/film/FilmPage.tsx
@@ -1,10 +1,25 @@
 import { useParams } from "react-router-dom";
 import { data } from "../../moks.js";
+import "./FilmPage.scss";
 const FilmPage = () => {
   const filmData = data.docs[0];
   console.log(filmData);
   const { filmId } = useParams();
-  return <div>{filmId}</div>;
+  return (
+    <div className="film-page">
+      <div className="image-section">
+        {filmData.poster?.url ? (
+          <img className="film-page__img" src={filmData.poster?.url} alt="" />
+        ) : (
+          <div className="no-img">No image</div>
+        )}
+        <button className="film-page__button">Добавить в избранное</button>
+      </div>
+      <div className="description-secioin">
+        <h3 className="film-page__title">Описание фильма:</h3>
+      </div>
+    </div>
+  );
 };
 
 export default FilmPage;
